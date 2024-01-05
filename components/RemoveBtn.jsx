ab462482c7df55219cc51e320a8b2013d9c608e1
rabnawaz-aptech <rabnawaz.aptech@gmail.com>
2024-01-06 00:39:20 +0500

    Updated

--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -9,7 +9,7 @@ export default function RemoveBtn({id}) {
     const confirmed = confirm("Are you sure?");
 
     if(confirmed){
-      const res = await fetch(`https://crud-pink-six.vercel.app/topics?id=${id}`, {
+      const res = await fetch(`https://crud-pink-six.vercel.app/api/topics?id=${id}`, {
         method: "DELETE",
       });
 
